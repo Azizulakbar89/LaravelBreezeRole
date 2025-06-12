a84df5a0a07e4e1f842406dcc26d96819cf7ba1c
Ananda Azizul Akbar <108566135+Azizulakbar89@users.noreply.github.com>
2025-06-12 22:22:11 +0700

    seeder

---
+++ database/seeders/userSeed.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Seeding;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeed extends Seeder
+{
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'admin',
+        ]);
+
+        User::create([
+            'name' => 'Teacher User',
+            'email' => 'teacher@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'teacher',
+        ]);
+
+        User::create([
+            'name' => 'Regular User',
+            'email' => 'user@example.com',
+            'password' => Hash::make('password'),
+            'role' => 'user',
+        ]);
+    }
+}
