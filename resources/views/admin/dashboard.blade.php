d815a3d0d05e12fc3b28972e17c77c41c3d799ea
Ananda Azizul Akbar <108566135+Azizulakbar89@users.noreply.github.com>
2025-06-12 22:00:28 +0700

    view antar role

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>User Dashboard</h1>
-        <p>Welcome, User!</p>
+        <h1>Admin Dashboard</h1>
+        <p>Welcome, Admin!</p>
     </div>
 @endsection
--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('content')
     <div class="container">
-        <h1>User Dashboard</h1>
-        <p>Welcome, User!</p>
+        <h1>Teacher Timetable</h1>
+        <p>Welcome, Teacher!</p>
     </div>
 @endsection
