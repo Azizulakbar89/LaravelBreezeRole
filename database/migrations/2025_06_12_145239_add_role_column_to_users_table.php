d07e2e2940179e3f2a16c8487d9fcac7a37b709d
Ananda Azizul Akbar <108566135+Azizulakbar89@users.noreply.github.com>
2025-06-12 21:56:07 +0700

    migrasi fix

--- database/migrations/2025_06_12_145239_add_role_column_to_users_table.php
+++ database/migrations/2025_06_12_145239_add_role_column_to_users_table.php
@@ -4,19 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddRoleIdToUsersTable extends Migration
+return new class extends Migration
 {
-    public function up()
+    public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->foreignId('role_id')->constrained()->onDelete('cascade');
+            $table->string('role')->default('user'); // Default role is 'user'
         });
     }
 
-    public function down()
+    public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('role_id');
+            $table->dropColumn('role');
         });
     }
-}
+};
